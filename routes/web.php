100dc8bb336a871042618070fc0db92106a5daef
Buse Sukuşu <busesukusu@gmail.com>
2022-03-11 12:34:49 +0300

    created auth and roles

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +30,17 @@ Route::get('/products',function(){ return view('products');})->name('products');
 Route::get('/contact',function(){return view('contact');})->name('contact');
 Route::get("/contact/create",[ContactController::class,'create'])->name('create');
 Route::post("/contact",[ContactController::class,'store'])->name('contact-create');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix'=>'admin','middleware'=>'isAdmin','auth'],function(){
+  Route::get('/dashboard',[AdminController::class,'index'])->name('admin.dashboard');
+  Route::get('/profile',[AdminController::class,'profile'])->name('admin.profile');
+});
+
+Route::group(['prefix'=>'user','middleware'=>'isUser','auth'],function(){
+  Route::get('/dashboard',[UserController::class,'index'])->name('user.dashboard');
+  Route::get('/profile',[UserController::class,'profile'])->name('user.profile');
+});
